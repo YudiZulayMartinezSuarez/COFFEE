47060f73f339099afbb16ad0e0aa2f48e0ee89fa
Miguel Vasquez <miguelvasquezf8@gmail.com>
2023-02-06 07:58:21 -0500

    s

---
+++ app.py
@@ -0,0 +1,16 @@
+#sadsasdasd
+import random
+import string
+#hola
+i = 0
+
+def id_generator(size=6, chars=string.ascii_uppercase):
+   return ''.join(random.choice(chars) for _ in range(size))
+
+#print(id_generator())
+while True:
+    search = id_generator()
+    i = i + 1
+    print(search,'!' ,i)
+    if search == 'MIGUEL':
+        exit()
