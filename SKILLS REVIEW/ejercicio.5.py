6dbd861ab6d9b20a011780100e31b8432f79d544
YudiZulayMartinezSuarez <yzmartinez@gmail.com>
2023-02-07 13:16:43 -0500

    taller.1

--- SKILLS REVIEW/ejercicio.5.py
+++ SKILLS REVIEW/ejercicio.5.py
@@ -1,5 +1,16 @@
 """5. Enumere 5 tipos de datos en Python y suministre un valor de
 ejemplo de cada uno."""
 
+print("los cinco tipos de datos en python son: string, double, lista, cadena, entero")
 
+
+string = "zulay"
+list = ["audifonos", "aretes", "collar"]
+double = 44.67574546534322421
+entero =  44 
+cadena = hola = 'Hola "Pythonista"'
+hola_2 = 'Hola \'Pythonista\''
+hola_3 = "Hola 'Pythonista'"
+
+print(f"\n\t\t{string} \n\t\t{list} \n\t\t{double} \n\t\t{entero} \n\t\t{cadena} \n\t\t{hola} \n\t\t{hola_2} \n\t\t{hola_3}")
 
